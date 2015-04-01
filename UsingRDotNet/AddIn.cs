3a0634b6cc4d815658c4c0e8261611cfd8e65568
Govert van Drimmelen <govert@icon.co.za>
2015-04-01 12:37:55 +0200

    Remove unused usings

--- UsingRDotNet/AddIn.cs
+++ UsingRDotNet/AddIn.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using ExcelDna.Integration;
 using ExcelDna.Logging;
 using RDotNet;
