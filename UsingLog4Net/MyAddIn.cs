aa7bdd75539ff281f425974dfc9ba85bab87b148
Govert van Drimmelen <govert@icon.co.za>
2015-04-02 23:45:21 +0200

    Add UsingLog4Net sample

---
+++ UsingLog4Net/MyAddIn.cs
@@ -0,0 +1,18 @@
+﻿using ExcelDna.Integration;
+using log4net;
+
+[assembly:log4net.Config.XmlConfigurator()]
+
+namespace UsingLog4Net
+{
+    public static class MyAddIn
+    {
+        static ILog Logger = LogManager.GetLogger("MyAddIn");
+
+        public static double AddThemAndLog(double x, double y)
+        {
+            Logger.Debug(">>>>> AddThemAndLog called.");
+            return x + y;
+        }
+    }
+}
