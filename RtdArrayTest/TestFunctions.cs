e2c0681287b096e25e0e428c3bf39ca7613230b4
Govert van Drimmelen <govert@icon.co.za>
2015-07-10 21:41:09 +0200

    Add problem function (with IsMacroType=true)

--- RtdArrayTest/TestFunctions.cs
+++ RtdArrayTest/TestFunctions.cs
@@ -25,5 +25,26 @@ namespace RtdArrayTest
             result[1, 0] = parts[1];
             return result;
         }
+
+        // NOTE: This is that problem case discussed in https://groups.google.com/d/topic/exceldna/62cgmRMVtfQ/discussion
+        // It seems that the DisconnectData will not be called on updates triggered by parameters from the sheet
+        // for RTD functions that are marked IsMacroType=true.
+        [ExcelFunction(IsMacroType=true)]
+        public static object RtdArrayTestMT(string prefix)
+        {
+            object rtdValue = XlCall.RTD("RtdArrayTest.TestRtdServer", null, "X" + prefix);
+
+            var resultString = rtdValue as string;
+            if (resultString == null)
+                return rtdValue;
+
+            // We have a string value, parse and return as an 2x1 array
+            var parts = resultString.Split(';');
+            Debug.Assert(parts.Length == 2);
+            var result = new object[2, 1];
+            result[0, 0] = parts[0];
+            result[1, 0] = parts[1];
+            return result;
+        }
     }
 }
