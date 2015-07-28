6721160107b456e785666f0d2e583a00e804aa3d
Govert van Drimmelen <govert@icon.co.za>
2015-07-28 23:47:46 +0200

    Add AsyncAwaitMacro sample
    Shows how the ExcelAsyncUtil.QueueAsMacro mechanism can be used to
    implement async macros with the C# async/await mechanism.

---
+++ AsyncAwaitMacro/AsyncMacros.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using ExcelDna.Integration;
+
+namespace AsyncAwaitMacro
+{
+    public static class AsyncMacros
+    {
+        static dynamic Application = ExcelDnaUtil.Application;
+
+        [ExcelCommand(MenuName = "AsyncAwaitMacros", MenuText = "DumpDataSlowly")]
+        public static void DumpDataSlowly()
+        {
+            ExcelAsyncTask.Run(DumpDataSlowlyImpl);
+        }
+
+        static async Task DumpDataSlowlyImpl()
+        {
+            // All the code here will run on the main thread - (though any Tasks run internally may do work on separate threads)
+
+            Debug.Print("1> {0}", Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            Application.Range["A1"].Value = DateTime.Now.ToString("> HH:mm:ss");
+
+            Debug.Print("2> {0}", Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            Application.Range["A2"].Value = DateTime.Now.ToString("> HH:mm:ss");
+
+            Debug.Print("3> {0}", Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            Application.Range["A3"].Value = DateTime.Now.ToString("> HH:mm:ss");
+
+            Debug.Print("4> {0}", Thread.CurrentThread.ManagedThreadId);
+        }
+
+    }
+}
---
+++ AsyncAwaitMacro/ExcelAsyncTask.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExcelDna.Integration
+{
+    public static class ExcelAsyncTask
+    {
+        public static Task Run(Func<Task> function)
+        {
+            return Task<Task>.Factory.StartNew(function, CancellationToken.None, TaskCreationOptions.DenyChildAttach, ExcelAsyncTaskScheduler.Instance).Unwrap();
+        }
+    }
+
+    class ExcelAsyncTaskScheduler : TaskScheduler
+    {
+        protected override void QueueTask(Task task)
+        {
+            ExcelAsyncUtil.QueueAsMacro(PostCallback, task);
+        }
+
+        void PostCallback(object obj)
+        {
+            Task task = (Task)obj;
+            TryExecuteTask(task);
+        }
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            // TODO: We might add a mechanism that tries this...
+            return false;
+        }
+
+        protected override IEnumerable<Task> GetScheduledTasks()
+        {
+            return null;
+        }
+
+        internal static ExcelAsyncTaskScheduler Instance = new ExcelAsyncTaskScheduler();
+    }
+}
