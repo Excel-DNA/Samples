c7a46a5e148c2bba92adc8d2cd4a6a8fdce08f0e
Govert van Drimmelen <govert@icon.co.za>
2020-03-02 01:41:25 +0200

    Add HttpClient async project

---
+++ HttpClient/AddIn.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExcelDna.Integration;
+using ExcelDna.Registration;
+
+namespace HttpClientSample
+{
+    public class AddIn : IExcelAddIn
+    {
+        public void AutoOpen()
+        {
+            RegisterFunctions();
+        }
+
+        public void AutoClose()
+        {
+        }
+
+        void RegisterFunctions()
+        {
+            ExcelRegistration.GetExcelFunctions()
+                             .ProcessAsyncRegistrations(nativeAsyncIfAvailable: false)
+                             .RegisterFunctions();
+        }
+    }
+}
---
+++ HttpClient/Functions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ExcelDna.Integration;
+
+namespace HttpClientSample
+{
+    public static class Functions
+    {
+        static HttpClient httpClient = new HttpClient();
+
+        [ExcelFunction]
+        public static async Task<string> httpGetString(string uri)
+        {
+            return await httpClient.GetStringAsync(uri);
+        }
+    }
+}
