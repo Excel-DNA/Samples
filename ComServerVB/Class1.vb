89675731f250bf6169b9e73eaa7fa33fb9680355
rkapl123 <rolandk@kapl.org>
2021-03-12 18:10:16 +0100

    Add ComServer example for VB

---
+++ ComServerVB/Class1.vb
@@ -0,0 +1,27 @@
+﻿Imports ExcelDna.Integration
+Imports ExcelDna.ComInterop
+Imports System.Runtime.InteropServices
+
+<ClassInterface(ClassInterfaceType.AutoDispatch)>
+<ProgId("ComAddin.FunctionLibrary")>
+<ComVisible(True)>
+Public Class AccessibleFunctions
+
+    Public Function add(x As Double, y As Double)
+        Return x + y
+    End Function
+
+End Class
+
+<ComVisible(False)>
+Public Class AddInEvents
+    Implements IExcelAddIn
+
+    Public Sub AutoOpen() Implements IExcelAddIn.AutoOpen
+        ComServer.DllRegisterServer()
+    End Sub
+
+    Public Sub AutoClose() Implements IExcelAddIn.AutoClose
+        ComServer.DllUnregisterServer()
+    End Sub
+End Class
---
+++ ComServerVB/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die einer Assembly zugeordnet sind.
+
+' Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("ClassLibrary1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ClassLibrary1")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID wird für die typelib-ID verwendet, wenn dieses Projekt für COM verfügbar gemacht wird.
+<Assembly: Guid("1a294cb3-e3dc-4c09-8cf3-b83aa5b6f0cb")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+' indem Sie "*" wie unten gezeigt eingeben:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
