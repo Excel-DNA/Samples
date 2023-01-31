aeb81b0ba2883960b058842eb8535eb3db01aae1
Andrew Kittredge <andrewlkittredge@gmail.com>
2023-01-31 12:46:08 -0500

    batching sample

---
+++ BatchedFunctionCalls/FunctionParams.cs
@@ -0,0 +1,12 @@
+﻿namespace BatchedFunctionCalls
+{
+    internal class FunctionParams
+    {
+        public string Ticker;
+        public int Year;
+
+        public readonly TaskCompletionSource<object> result = new();
+
+        public override string ToString() => $"FunctionParams {Ticker}-{Year}";
+    }
+}
