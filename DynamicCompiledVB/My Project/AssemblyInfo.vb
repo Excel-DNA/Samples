54dbdbba9d6ffbae9a557cab7ff0b1824e8606dc
sagi <icicles_truth.0f@icloud.com>
2023-09-07 22:14:35 +0800

    DynamicCompiledVB:  Update to SDK-style project file

--- DynamicCompiledVB/My Project/AssemblyInfo.vb
+++ DynamicCompiledVB/My Project/AssemblyInfo.vb
@@ -8,10 +8,10 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("DynamicCompiledVB")>
+'<Assembly: AssemblyTitle("DynamicCompiledVB")>
 <Assembly: AssemblyDescription("")>
-<Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("DynamicCompiledVB")>
+'<Assembly: AssemblyCompany("")>
+'<Assembly: AssemblyProduct("DynamicCompiledVB")>
 <Assembly: AssemblyCopyright("Copyright ©  2019")>
 <Assembly: AssemblyTrademark("")>
 
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+'<Assembly: AssemblyVersion("1.0.0.0")>
+'<Assembly: AssemblyFileVersion("1.0.0.0")>
