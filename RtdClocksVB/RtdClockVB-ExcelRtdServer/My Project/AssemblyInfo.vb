fdd4c6fefd1df034c71157635a8c800eb980a30d
sagi <icicles_truth.0f@icloud.com>
2023-09-09 14:30:55 +0800

    RtdClocksVB: Update SDK-style project file

--- RtdClocksVB/RtdClockVB-ExcelRtdServer/My Project/AssemblyInfo.vb
+++ RtdClocksVB/RtdClockVB-ExcelRtdServer/My Project/AssemblyInfo.vb
@@ -8,10 +8,10 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("RtdClockVB-ExcelRtdServer")>
+'<Assembly: AssemblyTitle("RtdClockVB-ExcelRtdServer")>
 <Assembly: AssemblyDescription("")>
-<Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("RtdClockVB-ExcelRtdServer")>
+'<Assembly: AssemblyCompany("")>
+'<Assembly: AssemblyProduct("RtdClockVB-ExcelRtdServer")>
 <Assembly: AssemblyCopyright("Copyright ©  2023")>
 <Assembly: AssemblyTrademark("")>
 
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+'<Assembly: AssemblyVersion("1.0.0.0")>
+'<Assembly: AssemblyFileVersion("1.0.0.0")>
