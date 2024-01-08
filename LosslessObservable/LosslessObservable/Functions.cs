a54853bf94bf392ec8a6df799625c997a9d29133
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2024-01-08 17:58:38 +0500

    Created LosslessObservable project.

---
+++ LosslessObservable/LosslessObservable/Functions.cs
@@ -0,0 +1,22 @@
+﻿using ExcelDna.Integration;
+
+namespace LosslessObservable
+{
+    public static class Functions
+    {
+        public static object ObservableSequence()
+        {
+            return ExcelAsyncUtil.Observe("ObservableSequence", null, ExcelObservableOptions.Lossless, () => new ObservableSequence());
+        }
+
+        public static object ObservableTimedSequence()
+        {
+            return ExcelAsyncUtil.Observe("ObservableTimedSequence", null, ExcelObservableOptions.Lossless, () => new ObservableTimedSequence());
+        }
+
+        public static object LosslessClock()
+        {
+            return ExcelAsyncUtil.Observe("LosslessClock", null, ExcelObservableOptions.Lossless, () => new LosslessClock());
+        }
+    }
+}
---
+++ LosslessObservable/LosslessObservable/ObservableSequence.cs
@@ -0,0 +1,28 @@
+﻿using ExcelDna.Integration;
+
+namespace LosslessObservable
+{
+    internal class ObservableSequence : IExcelObservable
+    {
+        public IDisposable Subscribe(IExcelObserver observer)
+        {
+            for (int i = 0; i <= 5; ++i)
+                observer.OnNext(i.ToString());
+
+            observer.OnCompleted();
+
+            return new ActionDisposable();
+        }
+
+        private class ActionDisposable : IDisposable
+        {
+            public ActionDisposable()
+            {
+            }
+
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
