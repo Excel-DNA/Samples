c0e69c4195b41707ef2b141ce0d4c2c2b9cbb19b
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-02-09 10:48:51 +0500

    Updated Registration.Sample.VisualBasic.

--- Registration.Sample.VisualBasic/My Project/AssemblyInfo.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System
-Imports System.Reflection
-Imports System.Runtime.InteropServices
-
-<Assembly: ComVisible(False)>
-
-'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("e9831df3-5557-494c-9da8-557d30621885")>
-
