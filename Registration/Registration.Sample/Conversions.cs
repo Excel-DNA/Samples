59b84ca8b32c8c7beaf411c8116d1d2fbaabee50
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-07-05 18:21:25 +0500

    Removed ExplicitRegistration.
    Added parameter conversion for custom types.

---
+++ Registration/Registration.Sample/Conversions.cs
@@ -0,0 +1,19 @@
+﻿using ExcelDna.Integration;
+
+namespace Registration.Sample
+{
+    public class Conversions
+    {
+        [ExcelParameterConversion]
+        public static TestType1 ToTestType1(string value)
+        {
+            return new TestType1(value);
+        }
+
+        [ExcelParameterConversion]
+        public static TestType2 TestType2FromTestType1(TestType1 value)
+        {
+            return new TestType2(value);
+        }
+    }
+}
