f93a8e32d67cbe80939225db41a90d6371247bed
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-07-06 10:04:05 +0500

    Added InstanceMemberRegistration.

---
+++ Registration/Registration.Sample/ExampleAddIn.cs
@@ -0,0 +1,17 @@
+﻿using ExcelDna.Integration;
+
+namespace Registration.Sample
+{
+    public class ExampleAddIn : IExcelAddIn
+    {
+        public void AutoOpen()
+        {
+            // First example if Instance -> Static conversion
+            InstanceMemberRegistration.TestInstanceRegistration();
+        }
+
+        public void AutoClose()
+        {
+        }
+    }
+}
